744ce18f842aaa2a079f87ec39577fb7529941da
TheRaizer <aidan.fu000@gmail.com>
2024-07-03 18:25:59 -0400

    fix comments

--- entities/systems/gravity.go
+++ entities/systems/gravity.go
@@ -7,8 +7,8 @@ import (
 func ApplyGravity(dt uint64, rb *objs.RigidBody) {
 	dtSec := float32(dt)
 
-	// max downward velocity of 500
-	if rb.Velocity.Y <= 300 {
+	// control max downward velocity
+	if rb.Velocity.Y <= 400 {
 		rb.Velocity.Y += 9.81 * dtSec
 	}
 }
